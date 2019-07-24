9015e6e3f8d249b21a34b19ac7a56b587390bb6e
laurastewart115 <38572133+laurastewart115@users.noreply.github.com>
2019-07-24 09:13:51 -0400

    Added all classes

---
+++ Memory-Game/Models/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memory_Game.Models
+{
+    public enum Shape
+    {
+        CIRCLE,
+        SQUARE,
+        DIAMOND,
+        TRIANGLE,
+        TRAPEZOID,
+        STAR
+    }
+}
